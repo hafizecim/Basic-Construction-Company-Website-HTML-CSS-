89ea47ecef0cef03015728f91f5afc65dd431ffd
hafizecim <hfz.snyl@gmail.com>
2025-09-01 10:42:36 +0300

    Basic construction company website_2

---
+++ Proje2/js/script.js
@@ -0,0 +1,9 @@
+// fullPage.js başlatma
+new fullpage('#fullpage', {
+  autoScrolling: true,
+  navigation: true,
+  navigationPosition: 'right',
+  scrollingSpeed: 700
+});
+
+console.log("Sayfa yüklendi ve fullPage.js aktif.");
